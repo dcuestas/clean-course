054c9d6d8e8f03a49f66e51567a8e93087f824de
Daniel Cuesta <dcuestas@sia.es>
2022-06-22 16:49:28 +0200

    Ejercicio 6: classes

---
+++ src/clean-code/06-classes-a.ts
@@ -0,0 +1,63 @@
+(() => {
+
+    // No aplicando el principio de responsabilidad única
+
+    type Gender = 'M'|'F';
+
+    class Person {
+        constructor(
+            public name: string,
+            public gender: Gender,
+            public birthdate: Date
+        ){}
+    }
+
+    
+    class User extends Person {
+
+        public lastAccess: Date;
+
+        constructor(
+            public email: string,
+            public role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date
+        ) {
+            super(name, gender, birthdate);
+            this.lastAccess = new Date();
+        }
+
+        checkCredentials() {
+            return true;
+        }
+    }
+
+    class UserSettings extends User {
+        constructor(
+            public workingDirectory: string,
+            public lastOpenFolder: string,
+            email: string,
+            role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date
+        ){
+            super(email, role, name, gender, birthdate);
+        }
+    }
+
+    const userSettings = new UserSettings(
+        '/usr/home',
+        '/home',
+        'daniel@google.com',
+        'Admin',
+        'Daniel',
+        'M',
+        new Date('1993-02-20')
+    );
+    
+        console.log({userSettings});
+
+
+})();
